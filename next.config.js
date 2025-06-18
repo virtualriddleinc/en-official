05010522aeb7d4ab0749a7f241f1d54f811e65e8
Yavuz <yavuz@Yavuz-MacBook-Pro.local>
2025-06-18 15:28:33 +0300

    AWS Amplify yapılandırması eklendi

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    formats: ['image/webp'],
-  },
-  experimental: {
-    optimizeCss: true,
+    unoptimized: true,
   },
 }
 

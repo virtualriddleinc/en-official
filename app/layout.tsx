9a897599c59697d4170da808c0718271c8bfafb4
Virtual Riddle Inc <info@virtualriddle.com>
2026-01-18 03:59:36 +0300

    Add GTM container (GTM-PTXL5HBN) to English version
    - Add GTM base script in head section with nonce for CSP compliance
    - Add noscript iframe in body section for GTM fallback
    - GTM container ID: GTM-PTXL5HBN (same as Turkish version)
    - Maintains CSP compliance with nonce-based script execution

--- app/layout.tsx
+++ app/layout.tsx
@@ -162,6 +162,21 @@ export default async function RootLayout({
               }}
             />
             <script
+              id="gtm-base"
+              nonce={nonce}
+              suppressHydrationWarning
+              dangerouslySetInnerHTML={{
+                __html: `
+                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;var n=d.querySelector('[nonce]');
+                  n&&j.setAttribute('nonce',n.nonce||n.getAttribute('nonce'));f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-PTXL5HBN');
+                `,
+              }}
+            />
+            <script
               id="google-ads-script"
               nonce={nonce}
               suppressHydrationWarning
@@ -408,6 +423,11 @@ export default async function RootLayout({
         {/* CSS will be loaded automatically by Next.js */}
       </head>
       <body className="min-h-screen bg-white">
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PTXL5HBN" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+          }}
+        />
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
